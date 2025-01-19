702f7af6311fb6183537aa50e088acb9549de82a
Lihu Zur <>
2025-01-19 15:07:22 +0200

    fix

--- modules/financial_bot/financial_bot/chains.py
+++ modules/financial_bot/financial_bot/chains.py
@@ -227,7 +227,7 @@ class ContextExtractorChain(Chain):
         classification = response.choices[0].message.content.strip().replace(".", "")
         debug_print(f"[DEBUG] GPT Classification is: {classification} and after splitting it's {classification.split(',')} and number of found {level}s is {len(classification.split(','))}")
         
-        top_k_options = [opt.strip() for opt in classification.split(",")][:top_k_options]  # Split and limit to top k
+        top_k_options = [opt.strip() for opt in classification.split(",")][:top_k_level]  # Split and limit to top k
 
         debug_print(f"[DEBUG] user prompt :{user_prompt}, GPT classification result: {top_k_options}")
         return top_k_options
